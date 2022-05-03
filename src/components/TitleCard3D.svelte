9ca4c1c3918ad781e80dbbff33ec8a0346da2836
simondoesstuff <sw.simonwalker@gmail.com>
2022-05-03 01:24:20 -0600

    added some contact details

---
+++ src/components/TitleCard3D.svelte
@@ -0,0 +1,52 @@
+<script>
+    import {onDestroy, onMount} from "svelte";
+    import VanillaTilt from "vanilla-tilt";
+
+    let ele;
+
+    onMount(() => {
+        VanillaTilt.init(ele, {
+            speed: 1000,
+            max: 10,
+            perspective: 500,
+            scale: 1.2,
+            glare: true
+        });
+    })
+
+    onDestroy(() => {
+        ele?.vanillaTilt.destroy();
+    })
+</script>
+
+
+<div bind:this={ele} class="preserve-3d titleCard" on:click>
+  <div class="translate-z-30">
+    <h1 class="select-none">SIMON WALKER</h1>
+  </div>
+</div>
+
+
+<style>
+    .titleCard {
+        @apply p-7 px-14 border-white text-5xl cursor-grab shadow-2xl;
+        letter-spacing: 0.5rem;
+        text-rendering: geometricPrecision;
+        font-family: 'Kanit', sans-serif;
+        text-shadow: rgba(0, 0, 0, 0.5) 0.2rem 0.2rem 0.5rem;
+    }
+
+    .titleCard:hover {
+        @apply border-2;
+        background: rgb(0, 120, 164);
+        background: linear-gradient(0deg, rgb(0, 120, 164) 0%, rgb(81, 0, 181) 100%);
+    }
+
+    .translate-z-30 {
+        transform: translateZ(30px);
+    }
+
+    .preserve-3d {
+        transform-style: preserve-3d;
+    }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,27 +1,6 @@
 <script>
-    import VanillaTilt from "vanilla-tilt";
-    import { fly } from 'svelte/transition';
-    import {onDestroy, onMount} from "svelte";
-
-    let tiltElements;
-
-    onMount(() => {
-        tiltElements = document.querySelectorAll('.vanillatilt');
-
-        VanillaTilt.init(tiltElements, {
-            speed: 500,
-            max: 10,
-            perspective: 500,
-            scale: 1.2,
-            glare: true
-        });
-    })
-
-    onDestroy(() => {
-        tiltElements?.forEach(element => {
-            element.vanillaTilt.destroy();
-        });
-    })
+    import {fly} from 'svelte/transition';
+    import TitleCard3D from "../components/TitleCard3D.svelte";
 
     // --------------------------------------------------
 
@@ -29,28 +8,16 @@
 </script>
 
 
-<svelte:head>
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@300&display=swap" rel="stylesheet">
-</svelte:head>
-
-
-
 <main class="globallyCentered">
   <div>
 
-    <div class="preserve-3d titleCard vanillatilt" on:click={() => open = !open}>
-      <div class="translate-z-30">
-        <h1 class="select-none">SIMON WALKER</h1>
-      </div>
-    </div>
+    <TitleCard3D on:click={() => open = !open}/>
 
     {#if open}
-<!--      <div class="text-center mt-16">-->
-<!--        <div transition:fly={{ y: -100, duration: 2000 }}>simon@simonwakler.tech</div>-->
-<!--        <div transition:fly={{ y: -100, duration: 2000 }}>970-581-6899</div>-->
-<!--      </div>-->
+      <div class="text-center mt-5">
+        <div class="contactDetail" transition:fly={{ y: -50, duration: 1000 }}>simon@simonwalker.tech</div>
+        <div class="contactDetail" transition:fly={{ y: -50, duration: 1000, delay: 200 }}>970  581  6899</div>
+      </div>
     {/if}
 
   </div>
@@ -58,36 +25,20 @@
 
 
 <style>
-  :global(html, body) {
-    background-color: black;
-    height: 100%;
-    @apply text-white;
-  }
+    :global(html, body) {
+        background-color: black;
+        height: 100%;
+        @apply text-white;
+    }
 
-  .titleCard {
-      @apply p-7 px-14 border-white text-5xl cursor-grab;
-      letter-spacing: 0.5rem;
-      text-rendering: geometricPrecision;
-      font-family: 'Kanit', sans-serif;
-  }
+    .globallyCentered {
+        @apply grid place-items-center absolute top-0 left-0;
+        width: 100%;
+        height: 100%;
+    }
 
-  .titleCard:hover {
-      @apply border-2;
-      background: rgb(0, 120, 164);
-      background: linear-gradient(0deg, rgb(0, 120, 164) 0%, rgb(81, 0, 181) 100%);
-  }
-
-  .globallyCentered {
-    @apply grid place-items-center absolute top-0 left-0;
-    width: 100%;
-    height: 100%;
-  }
-
-  .translate-z-30 {
-    transform: translateZ(30px);
-  }
-
-  .preserve-3d {
-    transform-style: preserve-3d;
-  }
+    .contactDetail {
+        @apply mb-5;
+        letter-spacing: 0.5rem;
+    }
 </style>
