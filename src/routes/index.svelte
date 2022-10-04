5063b2f08c60d56eae2b11719def7dbb7ac9945a
simondoesstuff <sw.simonwalker@gmail.com>
2022-10-04 01:49:46 -0600

    spoooky background fade

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,5 @@
 <script>
-    import {fly} from 'svelte/transition';
+    import {fly, fade} from 'svelte/transition';
     import TitleCard3D from "../components/TitleCard3D.svelte";
 
     let open = false;
@@ -13,13 +13,27 @@
     </div>
 </main>
 
+<!--   Software dev details     -->
 {#if open}
   <div class="globallyCentered text-center -z-10">
     <div class="relative top-10">
-      <div class="font-bold p-2" transition:fly={{ y: -30, duration: 800, delay: 0 }}>Software Developer</div>
-      <div class="extraLetterSpacing p-2" transition:fly={{ y: -30, duration: 800, delay: 250 }}>simon@simonwalker.tech</div>
-      <div class="extraLetterSpacing p-2" transition:fly={{ y: -30, duration: 800, delay: 500 }}>970 581 6899</div>
+      <div class="shadow-2xl font-bold p-2" transition:fly={{ y: -30, duration: 800, delay: 0 }}>Software Developer</div>
+      <div class="shadow-2xl extraLetterSpacing p-2" transition:fly={{ y: -30, duration: 800, delay: 250 }}>simon@simonwalker.tech</div>
+      <div class="shadow-2xl extraLetterSpacing p-2" transition:fly={{ y: -30, duration: 800, delay: 500 }}>970 581 6899</div>
     </div>
+  </div>
+{/if}
+
+
+<!--    Jumpscare photo   -->
+{#if open}
+  <div class="globallyCentered -z-20 w-full transparencyGradient">
+    <img
+        src="./personalPicture.png"
+        class="relative -top-28"
+        transition:fade={{ duration: 1500, delay: 1000 }}
+        alt=""
+    >
   </div>
 {/if}
 
@@ -27,6 +41,10 @@
 <style>
     :global(body) {
         height: 100%;
+    }
+
+    .transparencyGradient {
+        mask-image: linear-gradient(to bottom, rgba(0, 0, 0, 1) 0%, transparent 65%);
     }
 
     .titleCard {
